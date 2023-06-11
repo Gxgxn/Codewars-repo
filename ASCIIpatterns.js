eae68756a8ba0b7a19fdbfbbe8699271176c904d
Gxgxn <gdsinghb99@gmail.com>
2023-06-11 01:03:02 -0300

    ASCIIpatterns

---
+++ ASCIIpatterns.js
@@ -0,0 +1,27 @@
+/**
+ * @param n -  Row/Col Count
+ * HallowSquare Shape
+ *****
+ *   *
+ *   *
+ *   *
+ *****
+ */
+function hollowSquare(n) {
+  let string = "";
+
+  for (let i = 0; i < n; i++) {
+    // external loop
+    for (let j = 0; j < n; j++) {
+      // internal loop
+      if (i === 0 || i === n - 1) {
+        string += "*";
+      } else if (j !== 0 && j !== n - 1) {
+        string += " ";
+      } else {
+        string += "*";
+      }
+    }
+    string += "\n";
+  }
+}
